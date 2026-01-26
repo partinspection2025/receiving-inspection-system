43b642f9372475a2702d09b5a00365645b0a9335
partinspection2025 <partinspection2025@gmail.com>
2026-01-26 15:30:16 +0700

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,18 @@
+from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+
+from models import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
+
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(password: str, hashed: str):
-    return pwd_context.verify(password, hashed)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
